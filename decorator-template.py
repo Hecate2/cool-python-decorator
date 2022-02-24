ddb8b09be835e56fd7a2b887df964c23c1bb5500
Hecate2 <2474101468@qq.com>
2022-02-24 11:43:53 +0800

    async version

--- decorator-template.py
+++ decorator-template.py
@@ -10,8 +10,9 @@ class _Decorator:
     def __call__(self, *func_args, **func_kwargs):
         print('enter')
         print(self.decorator_args, self.decorator_kwargs)
-        self.decorated_func(*func_args, **func_kwargs)
+        result = self.decorated_func(*func_args, **func_kwargs)
         print('exit')
+        return result
 
     @classmethod
     def decorator(cls, *decorator_args, **decorator_kwargs):
@@ -26,7 +27,21 @@ class _Decorator:
 decorator = _Decorator.decorator
 
 
+class _AsyncDecorator(_Decorator):
+    async def __call__(self, *func_args, **func_kwargs):
+        print('async enter')
+        print(self.decorator_args, self.decorator_kwargs)
+        result = await self.decorated_func(*func_args, **func_kwargs)
+        print('async exit')
+        return result
+
+
+async_decorator = _AsyncDecorator.decorator
+
+
 if __name__ == '__main__':
+    print('sync usage')
+    
     @decorator(1, tries=1, backoff=2)
     def do_something(*args, **kwargs):
         print(args, kwargs)
@@ -45,3 +60,27 @@ if __name__ == '__main__':
     do_something(1, 2, a=3, decorator_called_with='args')
     do_something_without_decorator_args(4, 5, content='content', decorator_called='without args')
     do_something_without_decorator_call(5, 6, without='decorator-call')
+
+    print()
+    print('###################################################')
+    print('async usage')
+    import asyncio
+
+    @async_decorator(1, tries=1, backoff=2)
+    async def do_something(*args, **kwargs):
+        print(args, kwargs)
+
+
+    @async_decorator()
+    async def do_something_without_decorator_args(*args, **kwargs):
+        print(args, kwargs)
+
+
+    @async_decorator
+    async def do_something_without_decorator_call(*args, **kwargs):
+        print(args, kwargs)
+
+
+    asyncio.run(do_something(1, 2, a=3, decorator_called_with='args'))
+    asyncio.run(do_something_without_decorator_args(4, 5, content='content', decorator_called='without args'))
+    asyncio.run(do_something_without_decorator_call(5, 6, without='decorator-call'))
